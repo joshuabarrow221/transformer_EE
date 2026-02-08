3967573694af568ee14920f7f5fe6245e9820253
joshuabarrow221 <jbarrow@umn.edu>
2026-02-08 18:47:11 +0000

    added new file vectors to run through

--- transformer_ee/inference/configuration_file_tools/make_batch_inference_configs.sh
+++ transformer_ee/inference/configuration_file_tools/make_batch_inference_configs.sh
@@ -10,15 +10,27 @@ ATM_FILES=(
   "Train_Atmospheric_Flat_Models_cborden.txt"
 )
 
-BEAM_FILES=(
+DUNE_BEAM_FLAT_FILES=(
   "Train_DUNEBeam_Flat_Models_rrichi.txt"
   "Train_DUNEBeam_Flat_Models_jbarrow.txt"
   "Train_DUNEBeam_Flat_Models_cborden.txt"
 )
 
+DUNE_BEAM_NAT_FILES=(
+  "Train_DUNEBeam_Nat_Models_rrichi.txt"
+  "Train_DUNEBeam_Nat_Models_jbarrow.txt"
+  "Train_DUNEBeam_Nat_Models_cborden.txt"
+)
+
+NOVA_BEAM_NAT_FILES=(
+  "Train_NOvABeam_Nat_Models_rrichi.txt"
+  "Train_NOvABeam_Nat_Models_jbarrow.txt"
+  "Train_NOvABeam_Nat_Models_cborden.txt"
+)
+
 OUTDIR="."
 
 python3 generate_batch_inference_configs.py \
-  --outdir "${OUTDIR}" \
-  --atm-files "${ATM_FILES[@]}" \
-  --beam-files "${BEAM_FILES[@]}"
+#  --outdir "${OUTDIR}" \
+#  --atm-files "${ATM_FILES[@]}" \
+  --beam-files "${DUNE_BEAM_NAT_FILES[@]}"
